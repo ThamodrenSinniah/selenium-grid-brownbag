7d5081fe604b8e5631417cf805e7ea90c2b9eadb
Thamodren Sinniah <thamodren.sinniah@duerrdental.com>
2022-09-29 17:56:13 +0800

    fix

---
+++ lib/desired_capabilities.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# prefs
+CHROME_DESIRED_CAPABILITIES = {
+    'browserName': 'chrome',
+    'maxInstances': '5'
+}
+
+# profile
+FIREFOX_DESIRED_CAPABILITIES = {
+    'browserName': 'firefox',
+    'maxInstances': '5',
+    'intl.accept_languages': 'En'
+}
+
